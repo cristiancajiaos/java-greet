fdd94d45281aa0d05603d76ca4a694f51366a433
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-19 10:23:35 -0400

    Primer commit

---
+++ src/main/java/com/example/javagreet/JavaGreetApplication.java
@@ -0,0 +1,13 @@
+package com.example.javagreet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGreetApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JavaGreetApplication.class, args);
+  }
+
+}
