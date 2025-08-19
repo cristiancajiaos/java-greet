d779b502b961e1f642cbf482ecb0fb023d498d3b
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-19 10:38:58 -0400

    Creada solicitud POST /greet para obtener saludo a partir de un objeto con atributo message

--- src/main/java/com/example/javagreet/controller/GreetController.java
+++ src/main/java/com/example/javagreet/controller/GreetController.java
@@ -3,6 +3,8 @@ package com.example.javagreet.controller;
 import com.example.javagreet.entity.Greet;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,12 @@ public class GreetController {
   public ResponseEntity<Greet> getGreet() {
     Greet greet = new Greet("Hola, mundo!");
     return ResponseEntity.ok(greet);
+  }
+
+  @PostMapping
+  public ResponseEntity<Greet> postGreet(@RequestBody Greet greet) {
+    Greet newGreet = new Greet(greet.getMessage());
+    return ResponseEntity.ok(newGreet);
   }
 
 }
