6e8ecba158860e2ee886c40da962754d91fc5345
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-19 10:53:55 -0400

    Limpieza de controlador

--- src/main/java/com/example/javagreet/controller/GreetController.java
+++ src/main/java/com/example/javagreet/controller/GreetController.java
@@ -1,7 +1,6 @@
 package com.example.javagreet.controller;
 
 import com.example.javagreet.entity.Greet;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
